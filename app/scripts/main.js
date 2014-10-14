0a7ef5db0cadfd12cd6d3e4f733d3b3eee7af9a4
Mina Mikhail <mina@fightingtheboss.com>
2014-10-14 17:37:27 -0400

    Scale back basic transition example to starting point

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,29 +2,6 @@
 
 (function($) {
   $(document).ready(function() {
-    $('.animated').on('click', function(eventObject) {
-      eventObject.preventDefault();
-      $(this).toggleClass('is-animated');
-    });
 
-    $('#nav-toggle').on('click', function(eventObject) {
-      eventObject.preventDefault();
-      $(this).toggleClass('active');
-      $('#menu').toggleClass('active');
-    });
-
-    $('.panel').on('click', function(e) {
-      e.preventDefault();
-
-      if ( $(this).hasClass('panel-4') ) {
-        $(this).removeClass('active');
-        $('.panel-1').addClass('active');
-      } else {
-        $(this)
-          .removeClass('active')
-          .next()
-          .addClass('active');
-      }
-    });
   });
 })(jQuery);
